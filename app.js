253f4851c8ada3b26fe7d45ec973d53d12c7cc90
iamnothades <72025924+iamnothades@users.noreply.github.com>
2020-10-23 16:01:21 +0800

    test

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var budgetController = (function() {
     
-    var x = 23;
+    var x = "23";
     
-    var add = function(a) {
-        return x + a;
+    var add = function(a) {return x + a;
     }
-    
     return { 
-    publicTest: function(b) {
+        publicTest: function(b) {
         console.log(add(b));
     }
     }
