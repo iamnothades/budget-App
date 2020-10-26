8314b4cfb7c60d44d1be40cdd3f7daa1531cad5b
iamnothades <72025924+iamnothades@users.noreply.github.com>
2020-10-26 16:00:54 +0800

    wadfsadsfv

--- app.js
+++ app.js
@@ -21,6 +21,29 @@ var budgetController = (function () {
         }
     },
     
+    return  {
+        addItem: function (type,des,val) {
+            var newItem, ID;
+            //NOTE: Create new ID
+            if (data.allItems[type].length > 0) {
+                ID= data.allItems[type][data.allItems[type].length-1].id + 1
+            } else {
+                ID = 0
+            }
+            
+            //NOTE: Create new item based on 'inc' or 'exp' type
+            if (type === 'inc') {
+                newItem = new Income (ID,des,val);
+            } else if (type === 'exp') {
+                newItem = new Expense (ID,des,val)
+            } 
+            //NOTE: Push into new data structure
+            data.allItems[type].push(newItem)
+            //NOTE: Return new element
+            return newItem;
+        },
+        
+    }
 })();
 
 //NOTE: UI Controller
@@ -63,11 +86,14 @@ var controller = (function (budgetCtrl, UICntrl) {
             }
         });
     };
+
     var ctrlAddItem = function () {
+        var input,newItem;
         //TODO:
         //1. Get the filled input data
-        var input = UICntrl.getInput();
+        input = UICntrl.getInput();
         //2. Add the item to budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value)
         //3.Add new item to UI
         //4. Calc budget
         //5.Disp budget
