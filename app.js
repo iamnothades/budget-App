f0f6ee5820131d4acd1af9395aed6fe095f9358c
iamnothades <72025924+iamnothades@users.noreply.github.com>
2020-11-30 16:02:03 +0800

    Formatting Budget Numbers: String Manipulation

--- app.js
+++ app.js
@@ -142,6 +142,25 @@ var UIController = (function () {
         container: '.container',
         expensesPercLabel: '.item__percentage',
     };
+
+    var formatNumber = function (num, type) {
+        var numSplit, int, dec, type;
+        num = Math.abs(num);
+        num = num.toFixed(2);
+        numSplit = num.split('.');
+        int = numSplit[0];
+        if (int.legth > 3) {
+            int =
+                int.substr(0, int.length - 3) +
+                ',  ' +
+                int.substr(int.length - 3, 3); //input 2310, output 2,310
+        }
+        dec = numSplit[1];
+
+        type === 'exp' ? (sign = '-') : (sign = '+');
+
+        return (type === 'exp' ? '-' : '+') + '' + int + '.' + dec;
+    };
     return {
         getInput: function () {
             return {
@@ -156,12 +175,18 @@ var UIController = (function () {
         },
 
         displayBudget: function (obj) {
-            document.querySelector(DOMStrings.budgetLabel).textContent =
-                obj.budget;
-            document.querySelector(DOMStrings.incomeLabel).textContent =
-                obj.totalInc;
-            document.querySelector(DOMStrings.expensesLabel).textContent =
-                obj.totalExp;
+            var type;
+            obj.budget > 0 ? (type = 'inc') : (type = 'exp');
+
+            document.querySelector(
+                DOMStrings.budgetLabel
+            ).textContent = formatNumber(obj.budget);
+            document.querySelector(
+                DOMStrings.incomeLabel
+            ).textContent = formatNumber(obj.totalInc, 'inc');
+            document.querySelector(
+                DOMStrings.expensesLabel
+            ).textContent = formatNumber(obj.totalExp, 'exp');
 
             if (obj.percentage > 0) {
                 document.querySelector(DOMStrings.percentageLabel).textContent =
@@ -233,7 +258,7 @@ var UIController = (function () {
             //2)Replace the placeholder text with some actual data
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
-            newHtml = newHtml.replace('%value%', obj.value);
+            newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
 
             //3)Insert HTML into DOM
             document
